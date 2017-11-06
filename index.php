86253b0b1e42af39ac215b675dc06cad4c4a28ba
Rochelle Lewis <rlewis37@cnm.edu>
2017-11-06 13:33:45 -0700

    add navbar

--- index.php
+++ index.php
@@ -16,6 +16,53 @@
 		<title>Bootstrap 4 Challenge Demo</title>
 	</head>
 	<body>
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#">Bootstrap Challenge</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
 
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav ml-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link 1</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link 2</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</header>
+
+		<section>
+			<div class="container">
+
+			</div>
+		</section>
+
+		<section>
+			<div class="container">
+
+			</div>
+		</section>
+
+		<section>
+			<div class="container">
+
+			</div>
+		</section>
+
+		<footer>
+			<div class="container">
+
+			</div>
+		</footer>
 	</body>
 </html>
